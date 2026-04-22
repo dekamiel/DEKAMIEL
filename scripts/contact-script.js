b64805ff7a51041becbe36f484bae8e523fd5e86
dekamiel <send@dekamiel.com>
2026-04-22 20:39:56 +0200

    #Update_2026_04_22
    New website structure, layout, font and video.

--- scripts/contact-script.js
+++
@@ -1,7 +0,0 @@
-function openContact() {
-    document.getElementById("contactForm").style.height = "100%";
-}
-
-function closeContact() {
-    document.getElementById("contactForm").style.height = "0%";
-}
--- scripts/scroller-script.js
+++
@@ -1,5 +0,0 @@
-window.addEventListener('scroll', function() {
-    var scrolled = window.pageYOffset;
-    var video = document.getElementById('video');
-    video.style.transform = 'translateY(' + scrolled * 0.4 + 'px)';
-  });
